34e3b748b67f98890fbccdfce23b142bfd88254f
Trust-in-depth <kurtayemine713@gmail.com>
2025-11-06 21:13:30 +0300

    Add baykus-sinyal worker

---
+++ Workers/baykus_sinyal/src/room.ts
@@ -0,0 +1,13 @@
+export class Room {
+  state: DurableObjectState;
+  env: Env;
+
+  constructor(state: DurableObjectState, env: Env) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Room state active");
+  }
+}
